c1cb4a5373e4c98380bda6c74a1fca1ec291a9ce
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-23 10:06:09 -0500

    init in src routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginHomeComponent } from './login/login-home/login-home.component';
+import { RegisterComponent } from './login/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', 
+        component: LoginHomeComponent 
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path:'**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
