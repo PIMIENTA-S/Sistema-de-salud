4d7e07e7573122fb50ff3f1957356d61aca28a41
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-23 14:01:14 -0500

    update changes in backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule],
+  providers:[MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers:[
+    BrowserAnimationsModule,
+    provideHttpClient(),
+    ...appConfig.providers
+  ]
+}
+)
   .catch((err) => console.error(err));
