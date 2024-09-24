a0177cae20c43ffdd3eecfbf669b99d04bd7a369
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-24 13:33:10 -0500

    add changes in contact component, TO-DO: footer

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styles:``
 })
 export class ContactComponent {
 
