44baf950014212b08587823f6138df1f7f9cc29f
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-24 13:31:49 -0500

    add changes login module, routing and register

--- src/app/login/login-home/login-home.component.ts
+++ src/app/login/login-home/login-home.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-home',
@@ -23,7 +24,11 @@ export class LoginHomeComponent {
 
   })
 
-  constructor(private fb: FormBuilder){}
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService,
+    private router: Router
+  ){}
 
   get id(){
     return this.loginForm.controls['id']
@@ -31,6 +36,22 @@ export class LoginHomeComponent {
 
   get password(){
     return this.loginForm.controls['password']
+  }
+
+  loginUser(){
+    const {id, password} = this.loginForm.value;
+    this.authService.getUserById(Number(id)).subscribe(
+      response => {
+        if(response.length > 0 && response[0].password === password){
+          sessionStorage.setItem('id', id!.toString());
+          this.router.navigate(['/home']);
+        }else{
+          // MessageService TO-DO 
+        }
+      }
+    )
+    
+
   }
 
 }
--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { passwordMacthValidator } from '../../directive/password-match.directive';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interface/auth';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, HttpClientModule],
+  providers: [],
   templateUrl: './register.component.html',
-  styles:`
-  * 
-  {
-    font-family: 'FS-Joey', sans-serif;
-    font-weight: bold;
-  }
-  `
+  styles:``
 })
 export class RegisterComponent {
   registerForm = this.fb.group({
@@ -22,10 +22,22 @@ export class RegisterComponent {
     id: ['', Validators.required],
     password: ['', Validators.required],
     confirmPassword: ['', Validators.required]
+  },
+  {
+    validators: passwordMacthValidator
+  }
+)
 
-  })
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService, 
+    private messageService: MessageService,
+    private router: Router,
+  ){}
 
-  constructor(private fb: FormBuilder){}
+  ngOnInit() {
+    this.messageService.add({ severity: 'info', summary: 'Toast Test', detail: 'Esto es una prueba' });
+  }
 
   get fullName(){
     return this.registerForm.controls['fullName']
@@ -43,4 +55,22 @@ export class RegisterComponent {
     return this.registerForm.controls['confirmPassword']
   }
 
+  submitDetails(){
+    const postData = {...this.registerForm.value}
+    delete postData.confirmPassword;
+
+    this.authService.registerUser(postData as User).subscribe(
+      reponse => {
+        console.log(reponse);
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Registrado correctamente' });
+        this.router.navigate(['login'])
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Ha ocurrido un error' });
+      }
+      
+    )
+    
+  }
 }
+
