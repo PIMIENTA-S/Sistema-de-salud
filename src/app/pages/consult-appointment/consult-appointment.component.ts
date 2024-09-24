335bf3d39fb5c104e2b3e026a6e83d4161fb50b7
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-24 14:42:27 -0500

    add page consult-appointment and styles

--- src/app/pages/consult-appointment/consult-appointment.component.ts
+++ src/app/pages/consult-appointment/consult-appointment.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-consult-appointment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './consult-appointment.component.html',
   styleUrl: './consult-appointment.component.css'
 })
 export class ConsultAppointmentComponent {
+  appointmentType: 'presencial' | 'virtual' = 'presencial';
+  appointmentList = [
+    { type: 'Optometría', availability: 'Disponible' },
+    { type: 'Oncólogo', availability: 'Disponible' },
+    { type: 'Pediatra', availability: 'Disponible' },
+    { type: 'Odontología', availability: 'No disponible' },
+    { type: 'Cardiólogo', availability: 'Disponible' }
+  ];
+
+  setAppointmentType(type: 'presencial' | 'virtual') {
+    this.appointmentType = type;
+    // You can load different data based on the appointment type
+  }
+
+  bookAppointment(appointment: any) {
+    alert(`Cita reservada para: ${appointment.type}`);
+  }
 
 }
