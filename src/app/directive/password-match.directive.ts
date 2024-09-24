cfa63af568caa269761823456af6b6dbf524b8bc
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-24 13:29:46 -0500

    add directives for login | interface User

---
+++ src/app/directive/password-match.directive.ts
@@ -0,0 +1,14 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+
+export const passwordMacthValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null =>{
+
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if (!password || !confirmPassword){
+    return null;
+  }
+
+  return password.value === confirmPassword.value ? null : {passwordMismatch: true}
+}
