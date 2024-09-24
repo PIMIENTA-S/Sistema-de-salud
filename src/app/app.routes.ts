8b8e175f08d495112d006685e334c5091a702c04
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-24 14:44:22 -0500

    path for histoty and appoint | guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginHomeComponent } from './login/login-home/login-home.component';
 import { RegisterComponent } from './login/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ConsultAppointmentComponent } from './pages/consult-appointment/consult-appointment.component';
+import { HistoryComponent } from './pages/history-user/history.component';
 
 export const routes: Routes = [
     {
@@ -20,6 +22,14 @@ export const routes: Routes = [
     {
         path: 'nosotros',
         component: ContactComponent
+    },
+    {
+        path: 'citas',
+        component: ConsultAppointmentComponent
+    },
+    {
+        path: 'historia-clinica',
+        component: HistoryComponent
     },
     {
         path:'**',
