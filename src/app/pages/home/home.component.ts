3b5895af3a81f6426bd8c6de45d1899c5cd27420
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-04 15:34:28 -0500

    add goards into page and changes in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styles:``
 })
 export class HomeComponent {
+
 
 }
