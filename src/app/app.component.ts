16f89d164e639b096eb017fe5536018f83d39d75
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-04 16:16:41 -0500

    rensponsive nav add and update data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BtnCatComponent } from './shared/btn-cat/btn-cat.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, BtnCatComponent],
+  imports: [RouterOutlet, RouterLink, BtnCatComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sistema-salud';
+
+  isMenuOpen = false;
 
   constructor(private router: Router){}
 
