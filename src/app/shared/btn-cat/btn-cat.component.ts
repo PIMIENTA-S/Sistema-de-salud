2ebf93c418c086fa87081007f75df91fafe067c6
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-04 15:35:04 -0500

    module shared add for btn and footer

---
+++ src/app/shared/btn-cat/btn-cat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-btn-cat',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-cat.component.html',
+  styles:``
+})
+export class BtnCatComponent {
+
+}
