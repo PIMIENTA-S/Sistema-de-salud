22ec5d7893a2f599fa08e820e5830ed501f286e1
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-07 13:22:45 -0500

    update shared module

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'shared-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styles:``
 })
