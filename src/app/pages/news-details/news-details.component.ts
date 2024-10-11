dce55730c587ee1ddcd467896ee824256cf608bb
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-10 22:48:01 -0500

    details for news add

--- src/app/pages/news-details/news-details.component.ts
+++ src/app/pages/news-details/news-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-news-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './news-details.component.html',
   styles:``
 })
