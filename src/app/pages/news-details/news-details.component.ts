569a8c78993d3f04ef9278321d758848a8a25a0b
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-11 21:43:04 -0500

    add footer

--- src/app/pages/news-details/news-details.component.ts
+++ src/app/pages/news-details/news-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-news-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './news-details.component.html',
   styles:``
 })
