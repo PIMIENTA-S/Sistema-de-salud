e9935b806a3b034ecc014f04a605198e514c4127
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-26 23:42:52 -0500

    import bannerC into app.compo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BtnCatComponent } from './shared/btn-cat/btn-cat.component';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './shared/banner/banner.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, BtnCatComponent, CommonModule],
+  imports: [RouterOutlet, RouterLink, BtnCatComponent, CommonModule, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
